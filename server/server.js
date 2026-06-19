523fbc46e37a57b27ac9c6b4bf4a7180740c74ca
Maheen Fatima <f2024065405@umt.edu.pk>
2026-06-19 22:49:48 +0500

    Added preview URL to CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(cors({
     origin: [
-        'https://web-project-smartvote.vercel.app',  // ← YOUR ACTUAL URL
+        'https://web-project-smartvote-mamr8gwfq-saif23.vercel.app',  // ← YOUR ACTUAL URL
+        'https://web-project-smartvote.vercel.app',
         'https://smartvote.vercel.app',
         'http://localhost:3000'
     ],
